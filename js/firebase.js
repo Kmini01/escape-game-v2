76e0da2d19a79e55ba428adb003f410c3900711d
Kmini01 <mini48sky@gmail.com>
2026-08-03 14:09:59 +0900

    Set up Firebase and Firestore in firebase
    Initialized Firebase app and Firestore connection.

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,51 @@
+// ==========================
+// Firebase
+// ==========================
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+
+// Firebase 설정
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCCBok94XRoL368qt_sfM5fITdCmqXg71A",
+
+    authDomain: "mission-game-6a865.firebaseapp.com",
+
+    projectId: "mission-game-6a865",
+
+    storageBucket: "mission-game-6a865.firebasestorage.app",
+
+    messagingSenderId: "812766522494",
+
+    appId: "1:812766522494:web:5f7a7d2006732c36827c2f"
+
+};
+
+// Firebase 시작
+const app = initializeApp(firebaseConfig);
+
+// Firestore 연결
+const db = getFirestore(app);
+
+// 다른 파일에서 사용할 수 있도록 export
+export {
+
+    db,
+
+    collection,
+
+    addDoc,
+
+    getDocs,
+
+    serverTimestamp
+
+};
